486329f27ff4a57e6fdebb2d97be1812c7929c07
Alaa <lolomuhammd21@gmail.com>
2023-11-29 11:30:32 -0500

    1

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-import.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19/n')
+import.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
