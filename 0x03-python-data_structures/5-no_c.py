d56b31e40eab6af0a977acab5d135ba3f350169d
Alaa <lolomuhammd21@gmail.com>
2023-12-04 15:31:31 -0500

    Data strutures

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    vw = ""
+    for x in range(len(my_string)):
+        if (my_string[x] != 'c' and my_string[x] != 'c'):
+            return my_list
+    vw += my_string[x]
+    return vw
