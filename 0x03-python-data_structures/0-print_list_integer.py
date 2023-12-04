01fb8f2600be70b9f117d0179cf5e35881946a16
Alaa <lolomuhammd21@gmail.com>
2023-12-04 13:45:07 -0500

    Data strutures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if not my_list:
-        return None
-    for x in reversed(my_list):
+    for x in my_list:
         print("{:d}".format(x))
