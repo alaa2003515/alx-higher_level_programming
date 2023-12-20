ea8b33ad2f0c602df638cd38074e5e753fabb1cc
Alaa <lolomuhammd21@gmail.com>
2023-12-20 11:50:57 -0500

    1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=", ")
-print(i + 1)
+for number in range(100):
+    if (number != 99):
+        print("{}{}, ".format(int(number / 10), number % 10), end="")
+    else:
+        print("{}{}".format(int(number / 10), number % 10))
