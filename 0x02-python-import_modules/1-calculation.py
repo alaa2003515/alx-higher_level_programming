3a7fa0bb080be454d65e04a6e2eb3d00d24be908
Alaa <lolomuhammd21@gmail.com>
2023-12-20 12:17:33 -0500

    1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    i = 5
-    print("{} + {} = {}".format(a, i, add(a, i)))
-    print("{} - {} = {}".format(a, i, sub(a, i)))
-    print("{} * {} = {}".format(a, i, mul(a, i)))
-    print("{} / {} = {}".format(a, i, div(a, i)))
+import random
+number = random.randint(-10000, 10000)
+tempNum = number
+
+if number < 0:
+    number = -(number)
+
+lastDigit = number % 10
+if tempNum < 0:
+    number = tempNum
+    lastDigit = -(lastDigit)
+
+if lastDigit > 5:
+    string = "and is greater than 5"
+elif lastDigit == 0:
+    string = "and is 0"
+elif lastDigit < 6:
+    string = "and is less than 6 and not 0"
+
+print("Last digit of {:d} is {:d}".format(number, lastDigit), string)
